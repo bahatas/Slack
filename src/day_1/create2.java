06837b18a88b05b74e396a1cd5e951f3465aa48d
bahatas <bahattintastan@yandex.com>
2020-05-12 18:07:01 +0200

    add create2.java

---
+++ src/day_1/create2.java
@@ -0,0 +1,13 @@
+package day_1;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class create2 {
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println("deneme");
+        }
+        System.out.println("Denem sout dısı burası.");
+    }
+
+}
