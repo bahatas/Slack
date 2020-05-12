09064a15e69e99c3e10f056cb40a1479d6da99c3
bahatas <bahattintastan@yandex.com>
2020-05-12 17:42:12 +0200

    CreateChannel

---
+++ src/day_1/CreateChannel.java
@@ -0,0 +1,9 @@
+package day_1;
+
+public class CreateChannel {
+    public static void main(String[] args) {
+
+        //main met
+        System.out.println("crated channel under slack project 00");
+    }
+}
