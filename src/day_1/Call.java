75030e580661a6cf9980a25e17c46fa716f950bc
bahatas <bahattintastan@yandex.com>
2020-05-13 18:45:15 +0200

    add Call.java

---
+++ src/day_1/Call.java
@@ -0,0 +1,7 @@
+package day_1;
+
+public class Call {
+    public static void main(String[] args) {
+        //call suers able to see callers from here
+    }
+}
---
+++ src/day_1/Emoji.java
@@ -0,0 +1,18 @@
+package day_1;
+
+public class Emoji {
+    public static void main(String[] args) {
+
+        System.out.println("Emoji nerde ki");
+
+        public String Emojireturner(String first, String second){
+            String Emoji = "";
+            Emoji = first+second;
+
+
+            return Emoji;
+        }
+
+    }
+
+}
